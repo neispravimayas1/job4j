7901291b8d0dfb960a00827ed5c21f01ae607fbc
neispravimayas1 <neispravimayas1@gmail.com>
2020-02-11 15:28:31 +0300

    Максимум из двух чисел[#227611] исправленная

--- chapter_001/src/test/java/ru.job4j.condition/MaxTest.java
+++ chapter_001/src/test/java/ru.job4j.condition/MaxTest.java
@@ -5,12 +5,12 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
-        int result = Max.max(1,2);
-        assertThat(result, is(1));
+    public void whenMax2To1Then2() {
+        int result = Max.max(2,1);
+        assertThat(result, is(2));
     }@Test
-    public void whenMax2To1Then1() {
-        int result1 = Max.max(2,1);
+    public void whenMax1To2Then2() {
+        int result1 = Max.max(1,2);
         assertThat(result1, is(2));
     }@Test
     public void whenMax2To2Then2() {
