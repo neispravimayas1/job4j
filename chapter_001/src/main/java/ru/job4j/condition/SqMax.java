71389e7fe9bb1781b0f5624e98970c0f3068b1f8
neispravimayas1 <neispravimayas1@gmail.com>
2020-02-13 11:54:50 +0300

    Отладка программы в IDEA[#227610]

--- chapter_001/src/main/java/ru/job4j/condition/SqMax.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqMax.java
@@ -1,23 +1,23 @@
 package ru.job4j.condition;
 
 public class SqMax {
-
-  public static int max(int first, int second, int third, int forth) {
-      int result = third;
-      if (first > second) {
-                if (first > third) {
-                    if (first > forth) {
-                        result = third;
-                    }
-                }
-      } else if (second > third) {
-                if (second > forth) {
+    public static int max(int first, int second, int third, int forth) {
+        int result = third;
+        if (first > second) {
+            if (first > third) {
+                if (first > forth) {
                     result = first;
                 }
-      } else if (third > forth) {
+            }
+        } else if (second > third) {
+            if (second > forth) {
                 result = second;
-      }
-            return result;
+            }
+        } else if (third > forth) {
+            result = third;
+        }  else if (forth>third) {
+            result = forth;
         }
+        return result;
     }
-
+}
--- chapter_001/src/test/java/ru.job4j.condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru.job4j.condition/SqMaxTest.java
@@ -6,9 +6,28 @@ import static org.junit.Assert.assertThat;
 
 public class SqMaxTest {
     @Test
-    public void whenFirstMax() {
+    public void whenThirdMax() {
         SqMax check = new SqMax();
         int result = check.max(6, 4, 7, 5);
         assertThat(result, is(7));
+    }
+
+    @Test
+    public void whenFirstMax() {
+        SqMax check = new SqMax();
+        int result1 = check.max(4, 3, 2, 1);
+        assertThat(result1, is(4));
+    }
+    @Test
+    public void whenSecondMax() {
+        SqMax check = new SqMax();
+        int result1 = check.max(1, 4, 2, 3);
+        assertThat(result1, is(4));
+    }
+    @Test
+    public void whenFourthMax() {
+        SqMax check = new SqMax();
+        int result1 = check.max(1, 2, 3, 4);
+        assertThat(result1, is(4));
     }
 }
