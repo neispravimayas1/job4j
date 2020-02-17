224a65d736cf73994c6eabf19cdcf75504a4522e
neispravimayas1 <neispravimayas1@gmail.com>
2020-02-17 12:00:47 +0300

    Протеиновая диета.[#227684]

--- chapter_001/src/main/java/ru/job4j/loop/Fitness.java
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -3,9 +3,11 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while ((ivan *= 3) < (nik *= 2)) {
-             month++;
-               }
+        while (ivan<=nik) {
+            int jimIvan = ivan*=3;
+            int jimNik = nik*=2;
+            month++;
+                           }
 
        return month;
         }
