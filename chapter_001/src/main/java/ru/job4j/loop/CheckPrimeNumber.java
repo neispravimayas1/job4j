c827250588ab57dd7f2d5856710627aba11c1a51
neispravimayas1 <neispravimayas1@gmail.com>
2020-02-18 12:07:06 +0300

    Обновила файлы, вылезали на отправку

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -13,8 +13,8 @@ public class CheckPrimeNumber {
                 }
         }
         return prime;
-        }
     }
+}
 
 
 
--- chapter_001/src/test/java/ru.job4j.condition/MaxTest.java
+++ chapter_001/src/test/java/ru.job4j.condition/MaxTest.java
@@ -6,15 +6,15 @@ import static org.junit.Assert.assertThat;
 public class MaxTest {
     @Test
     public void whenMax2To1Then2() {
-        int result = Max.max(2,1);
+        double result = Max.max(2,1);
         assertThat(result, is(2));
     }@Test
     public void whenMax1To2Then2() {
-        int result1 = Max.max(1,2);
+        double result1 = Max.max(1,2);
         assertThat(result1, is(2));
     }@Test
     public void whenMax2To2Then2() {
-        int result2 = Max.max(2,2);
+        double result2 = Max.max(2,2);
         assertThat(result2, is(2));
 }
 }
