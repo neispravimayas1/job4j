e6f2a735c747682c511a2b0213e961454b016a84
neispravimayas1 <neispravimayas1@gmail.com>
2020-02-22 19:06:29 +0300

    5.8. JavaDoc. Code convention

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,12 @@
+package ru.job4j.calculate;
+
+/**
+ * Class Colculate создан для тренировки.
+ *  Method main.
+ * @param args - args.
+ */
+public class Calculate {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ * @author Dina Semenova (d.r.semenova@yandex.ru)
+ * @version 3.6.3
+ * @since 22/02/2020
+ */
+package ru.job4j.calculate;
