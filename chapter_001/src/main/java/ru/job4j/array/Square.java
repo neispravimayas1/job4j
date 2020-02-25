2079c766aeeab6f193f8fcdd596b01967a3a638e
neispravimayas1 <neispravimayas1@gmail.com>
2020-02-25 12:08:51 +0300

    6.0.5. Массивы в тестах.[#227638]

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -4,14 +4,16 @@ public class Square {
     public static int[] calculate(int bound) {
         int[] rst = new int[bound];
         for (int index = 0; index < bound; index++) {
-           rst[index] = index*index;
-                  }
+            rst[index] = index * index;
+        }
         return rst;
     }
     public static void main(String[] args) {
-        int[] array = calculate(4);
+        int[] array = calculate(3);
+        int[] array1 = calculate(5);
         for (int index = 0; index < array.length; index++) {
-            System.out.println(array[index]);
+            for (int index1 = 0; index1 < array1.length; index1++) {
+              }
         }
     }
 }
