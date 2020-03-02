29c10502ed5e58864173bc737b000c5a8acc1c4e
neispravimayas1 <neispravimayas1@gmail.com>
2020-03-02 15:20:31 +0300

    6.4.1. Слово заканчивается с ...[#227624]

--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -11,27 +11,37 @@ public class FindLoopTest {
         FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
         int value = 5;
-        int result = find.indexOf(input, value);
-        int expect = 0;
+
+        int expect = 0;int result = find.indexOf(input, value);
         assertThat(result, is(expect));
     }
-
     @Test
-    public void whenArrayHas3Then0() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {2, 7, 3};
-        int value = 3;
-        int result = find.indexOf(input, value);
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenFind10() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 10;
+        int start = 0;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = 2;
         assertThat(result, is(expect));
     }
-
     @Test
-    public void whenArrayHas4ThenNo() {
-        FindLoop find = new FindLoop();
-        int[] input = new int[] {2, 7, 3};
-        int value = 4;
-        int result = find.indexOf(input, value);
+    public void whenFindNon() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 7;
+        int start = 0;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
         assertThat(result, is(expect));
     }
