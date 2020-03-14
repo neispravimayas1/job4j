7f64a1d2855c346d33d8ee6882910345e71d162b
neispravimayas1 <neispravimayas1@gmail.com>
2020-03-14 15:02:39 +0300

    6.6.1. Двухмерный массив. Вывод на консоль.[#227642]

---
+++ chapter_001/src/main/java/ru/job4j/array/BarleyBreak.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class BarleyBreak {
+    public static void main(String[] args) {
+        int [][] table = new int [2][2];
+        table [0][0] = 1;
+        table [1][0] = 2;
+        table [2][0] = 3;
+        table [0][1] = 4;
+        table [1][1] = 5;
+        table [2][1] = 6;
+        table [0][2] = 7;
+        table [1][2] = 8;
+        table [1][2] = 9;
+
+    }
+}
